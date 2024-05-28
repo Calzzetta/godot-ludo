5147222afaac3c1ccf44ec8217f73c94fcf4af76
Calzzetta <calzzetta@gmail.com>
2024-05-27 23:16:04 -0300

    set dice faces as var

--- scripts/Dice.gd
+++ scripts/Dice.gd
@@ -2,15 +2,8 @@ extends Node2D
 class_name Dice
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var faces: int = 6
 
 
 func roll() -> int:
-	return randi_range(1, 6)
+	return randi_range(1, faces)
